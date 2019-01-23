7255856886c6fec0b74d8614c675a00465317ee1
tibovermunicht <tibovermunicht@gmail.com>
2019-01-23 11:32:51 +0100

    ChainIT-frontend

--- chainIt-frontend/src/app/home/home/module/module.component.ts
+++ chainIt-frontend/src/app/home/home/module/module.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { NgxChartsModule, CardSeriesComponent } from '@swimlane/ngx-charts';
+
 
 @Component({
   selector: 'app-module',
@@ -11,6 +12,19 @@ import { NgxChartsModule, CardSeriesComponent } from '@swimlane/ngx-charts';
 export class ModuleComponent implements OnInit {
   moduleName : String = "Module 123";
   cardSizes : any;
+  dataSource : any[] = [
+    {location: 'Geel', date: '23/1/2019-18:00', temp: 1.0079},
+    {location: 'Geel', date: '23/1/2019-19:00', temp: 4.0026},
+    {location: 'Geel', date: '23/1/2019-20:00', temp: 6.941},
+    {location: 'Geel', date: '23/1/2019-21:00', temp: 9.0122},
+    {location: 'Geel', date: '23/1/2019-22:00', temp: 10.811},
+    {location: 'Geel', date: '23/1/2019-23:00', temp: 12.0107},
+    {location: 'Geel', date: '24/1/2019-00:00', temp: 14.0067},
+    {location: 'Geel', date: '24/1/2019-01:00', temp: 15.9994},
+    {location: 'Geel', date: '24/1/2019-02:00', temp: 18.9984},
+    {location: 'Geel', date: '24/1/2019-03:00', temp: 20.1797},
+  ];
+  displayedColumns: string[] = ['location', 'date', 'temp'];
   
   cards = this.breakpointObserver.observe(Breakpoints.Large).pipe(
   map(({ matches }) => {
@@ -64,8 +78,9 @@ export class ModuleComponent implements OnInit {
     domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA','#0000b2 ']
   };
   
+  
   constructor(private breakpointObserver: BreakpointObserver) { }
-
+  
   ngOnInit() {
   }
 
