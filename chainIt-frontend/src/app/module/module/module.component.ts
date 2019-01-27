a858610fd1a696206804653741c5d03d9ecaafc5
tibovermunicht <tibovermunicht@gmail.com>
2019-01-27 17:49:03 +0100

    module filter

--- chainIt-frontend/src/app/module/module/module.component.ts
+++ chainIt-frontend/src/app/module/module/module.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { BreakpointState, BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { MatSidenav } from '@angular/material';
 import { FormControl } from '@angular/forms';
+import { SensorService } from 'src/app/services/sensor.service';
+import { ModuleInfoComponent } from './module-info/module-info.component';
+
 
 @Component({
   selector: 'app-module',
@@ -9,8 +12,10 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./module.component.css']
 })
 export class ModuleComponent implements OnInit {
-  modules : String[] = new Array('module111', 'module112', 'module113', 'module123' );
+  @ViewChild(ModuleInfoComponent) child;
+  modules : String[] = [];
   modeView : String;
+  sensorList: any;
 
   mode = this.breakpointObserver.observe(Breakpoints.Large)
   .subscribe((state: BreakpointState) => {
@@ -22,13 +27,26 @@ export class ModuleComponent implements OnInit {
     }    
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {  }
+  constructor(private breakpointObserver: BreakpointObserver, private sensorService: SensorService) {  }
 
   ngOnInit() {
+    this.sensorList = this.sensorService.getSensorList();
+    console.log(this.sensorList);
+    this.sensorList.subscribe(res => {
+      let list = res;
+        for (let sensor of list) {
+          this.modules.push(sensor.name);
+        }
+      });
+
   }
 
   windowResize(){
     window.dispatchEvent(new Event('resize'));
+  }
+
+  openSensorData(sensor : string){
+    this.child.getSensor(sensor);
   }
 
 }
--- chainIt-frontend/src/app/services/sensor.service.ts
+++ chainIt-frontend/src/app/services/sensor.service.ts
@@ -8,14 +8,21 @@ import { Observable } from 'rxjs';
 export class SensorService {
 
   sensorDataList;
+  sensorList;
   sensor = 'sensor1'
 
   constructor(private http: HttpClient) { }
 
 
   getSensorDataByName(sensor: string) {
-    this.sensorDataList = this.http.get('http://172.16.106.128:3000/api/Sensor/' + sensor);
+    //http://172.16.106.128:3000
+    this.sensorDataList = this.http.get('http://127.0.0.1:2024/api/Sensor/' + sensor);
     return this.sensorDataList;
+  }
+
+  getSensorList() {
+    this.sensorList = this.http.get('http://127.0.0.1:2024/api/Sensor');
+    return this.sensorList;
   }
 
 }
